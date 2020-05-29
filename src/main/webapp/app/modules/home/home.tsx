4340952f9e83f0fbb708071066ccee77b23676bb
sitedevente <martinjarod17@gmail.com>
2020-05-29 03:22:06 +0200

    Footer design and removed useless text on homepage

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -16,8 +16,7 @@ export const Home = (props: IHomeProp) => {
   return (
     <Row>
       <Col md="9">
-        <h2>Welcome, Java Hipster!</h2>
-        <p className="lead">This is your homepage</p>
+        <h2>Administration UI</h2>
         {account && account.login ? (
           <div>
             <Alert color="success">You are logged in as user {account.login}.</Alert>
@@ -35,51 +34,8 @@ export const Home = (props: IHomeProp) => {
               <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
             </Alert>
 
-            <Alert color="warning">
-              You do not have an account yet?&nbsp;
-              <Link to="/account/register" className="alert-link">
-                Register a new account
-              </Link>
-            </Alert>
           </div>
         )}
-        <p>If you have any question on JHipster:</p>
-
-        <ul>
-          <li>
-            <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">
-              JHipster homepage
-            </a>
-          </li>
-          <li>
-            <a href="http://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">
-              JHipster on Stack Overflow
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer">
-              JHipster bug tracker
-            </a>
-          </li>
-          <li>
-            <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-              JHipster public chat room
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/jhipster" target="_blank" rel="noopener noreferrer">
-              follow @jhipster on Twitter
-            </a>
-          </li>
-        </ul>
-
-        <p>
-          If you like JHipster, do not forget to give us a star on{' '}
-          <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-            Github
-          </a>
-          !
-        </p>
       </Col>
       <Col md="3" className="pad">
         <span className="hipster rounded" />
--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -5,10 +5,10 @@ import React from 'react';
 import { Col, Row } from 'reactstrap';
 
 const Footer = props => (
-  <div className="footer page-content">
+  <div className="footer page-content bg-primary">
     <Row>
       <Col md="12">
-        <p>A custom footer with no class neither style, fully updated by the developper of webapp react client app</p>
+        <p> &copy; Copyright Webuy </p>
       </Col>
     </Row>
   </div>
